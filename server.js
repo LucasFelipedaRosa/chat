d92614b2f422bb928089b58b42f708fafc03e097
Lucas Felipe da Rosa <lucasfelip98@gmail.com>
2020-08-03 18:28:32 -0300

    primeiros passos

---
+++ server.js
@@ -0,0 +1,21 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+
+const io = require('socket.io')(http)
+
+app.get('/', (req, res)=>{
+    res.sendfile(__dirname+'/index.html')
+})
+
+http.listen(3000, function(){
+    console.log('listening on port 3000')
+})
+
+io.on('connection', (socket)=>{
+    console.log('new connection', socket.id)
+    socket.on('msg',msg=>{
+        console.log(msg)
+        socket.broadcast.emit('msg', socket.id+ 'connected');
+    })
+})
+
