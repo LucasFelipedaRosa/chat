c532a1c13c6984de66967e6d03b86afa3a7fd0a0
Lucas Felipe da Rosa <lucasfelip98@gmail.com>
2020-08-19 17:19:02 -0300

    chat simples finalizado

--- server.js
+++ server.js
@@ -3,19 +3,38 @@ const http = require('http').createServer(app)
 
 const io = require('socket.io')(http)
 
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "test"
+});
+
 app.get('/', (req, res)=>{
-    res.sendfile(__dirname+'/index.html')
-})
+    con.connect(function (err) {
+        con.query("SELECT * FROM teste", function (err, result, fields) {
+            var resultado = result;
+            res.render(__dirname+'/index.html', {resultado})
+            //res.sendfile(__dirname+'/index.html')
+        });
+    });
+});
 
 http.listen(3000, function(){
     console.log('listening on port 3000')
 })
 
 io.on('connection', (socket)=>{
-    console.log('new connection', socket.id)
+    socket.join('room');
+    console.log('new connection', socket.id);
     socket.on('msg',msg=>{
         console.log(msg)
-        socket.broadcast.emit('msg', msg);
+        io.to('room').emit('msg', msg);
     })
 })
 
